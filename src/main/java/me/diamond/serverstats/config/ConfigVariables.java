d1b2e5a0a021862d008a84f8e5b72a9c33275cec
Diamond Iq <diamentowyiq@gmail.com>
2024-05-12 18:31:37 +0200

    Commit 1

---
+++ src/main/java/me/diamond/serverstats/config/ConfigVariables.java
@@ -0,0 +1,14 @@
+package me.diamond.serverstats.config;
+
+public enum ConfigVariables {
+    DATE,
+    HOUR,
+    DAY,
+    DAY_OF_THE_MONTH,
+    MONTH,
+    YEAR,
+    SERVER_NAME,
+    TOTAL_JOINS,
+    MOST_PLAYERS_AT_ONCE,
+    MOST_PLAYERS_AT_ONCE_TIME
+}
---
+++ src/main/java/me/diamond/serverstats/utils/TimeUtils.java
@@ -0,0 +1,28 @@
+package me.diamond.serverstats.utils;
+
+import java.sql.Date;
+import java.util.Calendar;
+
+public class TimeUtils {
+    public static long getStartOfDay(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar.getTimeInMillis();
+    }
+
+    // Utility method to get the end of the day as milliseconds since epoch
+    public static long getEndOfDay(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MILLISECOND, 999);
+        return calendar.getTimeInMillis() + 1; // Add 1 millisecond to get the end of the day
+    }
+
+}
